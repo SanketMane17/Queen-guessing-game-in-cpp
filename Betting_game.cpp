b084fea313fbaec1f1d5c61b2c6e1bd84bb19cf1
Sanket <msanket450@gmail.com>
2021-11-13 14:01:53 +0530

    Adding readable content

--- Betting_game.cpp
+++ Betting_game.cpp
@@ -23,27 +23,31 @@ void play(int bet){
         c[y] = temp; // Swaps characters at position x and y
     }
     int PlayersGuess;
-    cout<< "What's the position of the queen - 1, 2 & 3 = ";
+    cout<< "What's the position of the queen => 1, 2 & 3 = ";
     cin>> PlayersGuess;
     if(c[PlayersGuess - 1] == 'Q'){
         cash += 3*bet;
-        cout<< "You win!! Result = /" << c[0] << " " << c[1] << " "<<  c[2] << " / Total cash = " << cash << endl;
+        cout<< "You win!! Result = /" << c[0] << " " << c[1] << " "<<  c[2] << "/ Total cash = $" << cash << endl;
     }
     else{
         cash -= bet;
-        cout<< "You loose!! Result = /" << c[0] << " " << c[1] << " "<<  c[2] << " / Total cash = " << cash << endl;
+        cout<< "You loose!! Result = /" << c[0] << " " << c[1] << " "<<  c[2] << "/ Total cash = $" << cash  << endl;
     }
 }
 
 int main(){
     int bet;
     cout<< "***Welcome to the virtual casino***" << endl;
-    cout<< endl << "Total cash = " << cash << endl;
+    cout<< endl << "Total cash = $" << cash << endl;
     while (cash > 0)
     {
         cout<< "What's your bet? = $";
         cin>> bet;
-        if(bet == 0 || bet > cash) break;
+        if(bet == 0 || bet > cash)
+        {
+            cout<< "Your bet amout exceeded, Please enter bet amount less than your total cash" << endl;
+            break;
+        }
         play(bet);
     }
     
